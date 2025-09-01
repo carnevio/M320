b5c5d532fcfa023dd58e7cd158e8ec84338a758f
Nevio Carcangiu <nevio@carcangiu.net>
2025-09-01 10:43:02 +0200

    Initial commit

---
+++ OOP_Vertiefungsarbeit01/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace M320_Aufgaben
+{
+    internal class Program
+    {
+        
+        static void Main(string[] args)
+        {
+            Zylinder z1 = new Zylinder();
+            z1.setDurchmesser(10.5);
+            z1.setHoehe(20.5);
+            double v1 = z1.getVolumen();
+            double a1 = z1.getGrundflaeche();
+            double m1 = z1.getMantelflaeche();
+            double o1 = z1.getOberflaeche();
+            Console.WriteLine("Zylinder 01 : Volumen=" + v1.ToString() + " Oberflaeche=" + o1.ToString());
+            //---------------------------------------------
+            Zylinder z2 = new Zylinder(30.0, 15.5); //Param1 = Durchmesser, Param2 = Höhe
+            double o2 = z2.getOberflaeche();
+            double v2 = z2.getVolumen();
+            Console.WriteLine("Zylinder 02: Volumen=" + v2.ToString() + " Oberflaeche=" + o2.ToString());
+            //---------------------------------------------
+            double o3 = Zylinder.getOberflaeche(50.5, 20.0); //Param1 = Durchmesser, Param2 = Höhe
+            Console.WriteLine("Zylinder 03: Oberflaeche=" + o3.ToString());
+            //---------------------------------------------
+            Console.ReadLine();
+        }
+    }
+}
---
+++ OOP_Vertiefungsarbeit01/Zylinder.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace M320_Aufgaben
+{
+    public class Zylinder
+    {
+        private double durchmesser;
+        private double hoehe;
+        public Zylinder() 
+        {
+            this.durchmesser = 0.0;
+            this.hoehe = 0.0;
+        }
+
+        public Zylinder(double durchmesser, double hoehe)
+        {
+            this.durchmesser=durchmesser;
+            this.hoehe=hoehe;
+        }
+
+        public void setDurchmesser(double durchmesser)
+        {
+            this.durchmesser = durchmesser;
+        }
+
+        public void setHoehe(double hoehe)
+        {
+            this.hoehe = hoehe;
+        }
+
+        public double getGrundflaeche()
+        {
+            double radius = durchmesser / 2.0;
+            return Math.PI * radius * radius;
+        }
+
+        public double getMantelflaeche()
+        {
+            double radius = durchmesser / 2.0;
+            return 2 * Math.PI * radius * hoehe;
+        }
+
+        public double getOberflaeche()
+        {
+            return 2 * getGrundflaeche() + getMantelflaeche();
+        }
+
+        public double getVolumen()
+        {
+            return getGrundflaeche() * hoehe;
+        }
+
+        public static double getOberflaeche(double durchmesser, double hoehe)
+        {
+            double radius = durchmesser / 2.0;
+            double grundflaeche = Math.PI * radius * radius;
+            double mantelflaeche = 2 * Math.PI * radius * hoehe;
+            return 2 * grundflaeche + mantelflaeche;
+        }
+    }
+}
+
